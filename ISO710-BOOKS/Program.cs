528abfd5f37bef8c131ac0c36417a299e3195a5c
Johan809 <Johan809@hotmail.com>
2025-02-27 15:54:00 -0400

    upd: ultimos cambios (no los habia subido)

--- ISO710-BOOKS/Program.cs
+++ ISO710-BOOKS/Program.cs
@@ -5,11 +5,12 @@ using System.Globalization;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+// Configurar cultura
 var defaultCulture = new CultureInfo("es-ES");
 CultureInfo.DefaultThreadCurrentCulture = defaultCulture;
 CultureInfo.DefaultThreadCurrentUICulture = defaultCulture;
 
-// Add services to the container.
+// Agregar servicios al contenedor
 builder.Services.AddHttpClient<GoogleBooksService>();
 builder.Services.Configure<GoogleBooksSettings>(builder.Configuration.GetSection("ApiSettings"));
 builder.Services.AddControllersWithViews();
@@ -25,26 +26,43 @@ builder.Services.AddDbContext<Iso710Context>(options =>
 
 WebApplication app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuración del pipeline de solicitudes HTTP
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios,
-    // see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
+app.UseStaticFiles(); // Permitir servir archivos estáticos
 app.UseCors("PermitirTodo");
 app.UseRouting();
 app.UseAuthorization();
-app.UseEndpoints(endpoints => { _ = endpoints.MapControllers(); });
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}"
-);
+// **Servir archivos estáticos desde la carpeta ./app**
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "app")),
+    RequestPath = ""
+});
+
+// **Ruta predeterminada redirigida a Angular**
+app.UseEndpoints(endpoints =>
+{
+    _ = endpoints.MapControllers();
+
+    // Redireccionar cualquier otra ruta al index.html de Angular
+    _ = endpoints.MapFallbackToFile("index.html", new StaticFileOptions
+    {
+        FileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(
+            Path.Combine(Directory.GetCurrentDirectory(), "App/iso-books-ng/dist/iso-books-ng/browser"))
+    });
+});
+
+// **Eliminar la ruta MVC predeterminada**
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
